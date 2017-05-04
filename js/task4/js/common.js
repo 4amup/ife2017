ca808bc23baf1b2dbc205690c5686a6f0e353312
wozhizuipc <490398311@qq.com>
2017-05-04 11:31:53 +0800

    init task4

---
+++ js/task4/js/common.js
@@ -0,0 +1,13 @@
+window.onload = init();
+
+function init() {
+    
+}
+var leftIn = document.getElementById('leftIn');
+var rightIn = document.getElementById('rightIn');
+var leftOut = document.getElementById('leftOut');
+var rightOut = document.getElementById('rightOut');
+
+leftIn.onclick() = function() {
+    
+}
