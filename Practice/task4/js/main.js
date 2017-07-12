90a60300368e72b297235b459213a5befef01733
wozhizuipc <490398311@qq.com>
2017-07-12 17:18:06 +0800

    update 完成了绘图，使用了两层的canvas叠起来。

---
+++ Practice/task4/js/main.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+  let canvas = document.getElementById('canvas');
+  // 设定画布大小
+  canvas.width = 555;
+  canvas.height = 555;
+  let ctx=canvas.getContext("2d");
+  
+  // 绘制一个500大小的外框线，起点在(50,50)的坐标处
+  ctx.lineWidth = 2;
+  ctx.rect(50,50,500,500);
+  ctx.stroke();
+
+  // 添加坐标的文字
+  ctx.textAlign = 'center';
+  ctx.font="18px Arial";
+  for (let i=0; i<10; i++) {
+    ctx.fillText(`${i+1}`, 25+(i+1)*50 , 25);
+    ctx.fillText(`${i+1}`, 25, 25+(i+1)*50);
+  }
+
+  // 将里面的线画完
+  ctx.lineWidth = .3;
+  for(let i=0; i<9; i++) {
+    // 画竖线
+    ctx.beginPath();
+    ctx.moveTo(100+i*50, 50);
+    ctx.lineTo(100+i*50, 550);
+    ctx.stroke();
+
+    // 画横线
+    ctx.beginPath();
+    ctx.moveTo(50, 100+i*50);
+    ctx.lineTo(550, 100+i*50);
+    ctx.stroke();
+  }
+
+  let move = document.getElementById('move');
+  move.width = 550;
+  move.height = 550;
+  let moveCtx = move.getContext('2d');
+  // 为矩形填充颜色
+  moveCtx.fillStyle="red";
+  moveCtx.fillRect(0,0,50,50);
+  moveCtx.stroke();
+  moveCtx.fillStyle="blue";
+  moveCtx.fillRect(0,0,50,15);
+  moveCtx.stroke();
+
+}
