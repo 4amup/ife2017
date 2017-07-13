fa1b913f582495314dff9c1d793d5768f96dc1f2
wozhizuipc <490398311@qq.com>
2017-07-13 14:44:05 +0800

    done 完成任务，建立渲染模型theBlock。

--- Practice/task4/js/main.js
+++ Practice/task4/js/main.js
@@ -48,59 +48,135 @@ window.onload = function () {
     coordinate.x.push(50+i*50);
     coordinate.y.push(50+i*50);
   }
-  // 打印出坐标数组，没毛病
+  // 打印出坐标范围的数组，没毛病
   console.log(coordinate.x);
   console.log(coordinate.y);
 
-  // 移动矩形block的函数
-  function theBlock (x, y, z, w) {
+  // 按照当前坐标渲染出矩形block的函数
+  function theBlock (x, y, direction) {
     moveCtx.clearRect(0,0,550,550); // 清除画布
 
     moveCtx.fillStyle="red";
     moveCtx.fillRect(x,y,50,50);
     moveCtx.fillStyle="blue";
-    moveCtx.fillRect(x,y,z,w);
+    switch(direction) {
+      case 'up': {
+        moveCtx.fillRect(x,y,50,15);
+        break;
+      }
+      case 'left': {
+        moveCtx.fillRect(x,y,15,50);
+        break;
+      }
+      case 'down': {
+        moveCtx.fillRect(x,y+35,50,15);
+        break;
+      }
+      case 'right': {
+        moveCtx.fillRect(x+35,y,15,50);
+        break;
+      }
+    }
     moveCtx.stroke();
   }
 
   // 为小方块定义一个对象
-
-  // init第一次出现的位置
+  let direction = ['up', 'left', 'down', 'right'];
+  // init时，随机取小方块的x和y坐标及方向属性。
   let X = coordinate.x[Math.floor(Math.random()*coordinate.x.length)];
   let Y = coordinate.y[Math.floor(Math.random()*coordinate.y.length)];
-  let Z = 50;
-  let W = 15;
-  let block = {x: X, y: Y, z: Z, w: W};
-  // 初始化小方块
-  theBlock(block.x, block.y, block.z, block.w);
+  let D = direction[Math.floor(Math.random()*direction.length)];
+  let dircount = direction.indexOf(D);  // 取得方向属性初始值
+  
+  let block = {x: X, y: Y, direc: D}; // 为小方块赋值
+  theBlock(block.x, block.y, block.direc);  // 初始化小方块
 
   // 为执行按钮绑定事件
   document.getElementById('command').onclick = function () {
     let command = document.getElementById('input').value.toLowerCase();
-    if(!command) {
-      alert('请输入命令再执行');
-      return;
-    }
+
     switch(command) {
       case 'go': {
-        block.x += 0;
-        block.y -= 50;
-        if (!validate()) return;
-        theBlock(block.x, block.y, block.z, block.w);
+        goToWhere(block.direc);
+        filter()
+        theBlock(block.x, block.y, block.direc);
         break;
       }
       case 'tun lef': {
-        moveCtx.rotate(90*Math.PI/180);
-        theBlock(block.x, block.y, block.z, block.w);        
+        dircount += 1;
+        block.direc = direction[fixDirection(dircount)];        
+        theBlock(block.x, block.y, block.direc);        
+        break;
+      }
+      case 'tun rig': {
+        dircount -= 1;
+        block.direc = direction[fixDirection(dircount)];        
+        theBlock(block.x, block.y, block.direc);        
+        break;
+      }
+      case 'tun bac': {
+        dircount += 2;
+        block.direc = direction[fixDirection(dircount)];        
+        theBlock(block.x, block.y, block.direc);        
+        break;
+      }
+      case '' : {
+        alert('命令为空，请输入命令');
+        break;
+      }
+      default : {
+        alert('命令输入错误，请核对后再次输入');
+        input.value = '';
       }
     }
   }
 
-  // 验证小方块是否在坐标范围内
-  function validate () {
-    if(block.x>500 || block.y>500 || block.x<50 || block.y<50) {
-      return false;
+
+  // 处理block的方向，让方向绕圈
+  function fixDirection (count) {
+    if (count>3) {
+      count-=4;
     }
-    return true;
+    if (count<0) {
+      count+=4;
+    }
+    return dircount = count;
+  }
+  // 让go命令知道该往哪个方向走
+  function goToWhere (direction) {
+    switch (direction) {
+      case 'up': {
+        block.y -= 50;
+        break;
+      }
+      case 'left': {
+        block.x -= 50;
+        break;
+      }
+      case 'down': {
+        block.y += 50;
+        break;
+      }
+      case 'right': {
+        block.x += 50;
+        break;
+      }
+    }
+  }
+
+  // 若超过坐标范围，则强制将坐标置为边界值
+  function filter () {
+    if(block.x>500) {
+      block.x = 500;
+    }
+    if(block.y>500) {
+      block.y = 500;
+    }
+    if(block.x<50) {
+      block.x = 50;
+    }
+    if(block.y<50) {
+      block.y = 50;
+    }
   }
 }
