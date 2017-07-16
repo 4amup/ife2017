1a3a93cb143cade120094e55c5d5364a9061e511
wozhizuipc <490398311@qq.com>
2017-07-16 16:31:25 +0800

    update 暂时先不适用vue，实现了发牌和重置的功能。

--- Practice/texas1/js/main.js
+++ Practice/texas1/js/main.js
@@ -2,25 +2,44 @@ window.onload = () => {
   // 先定义一个用户组，单页同时显示，后续使用socket.io每页各自显示。
   let users = ['刘俊良', '汪峰', '章子怡', '陈玘', '林京来'];
 
-  // 定义代表扑克的数组，52张牌+1张背牌
-  const cards = Object.keys(String(new Array(53)));
-  console.log(Array.isArray(cards));
+  // 定义扑克数组
+  let cards = Object.keys(String(new Array(53)));
+  console.log(cards);
 
   // 每个用户一个div放牌，实验阶段每人发五张牌
   let pokerArea = document.getElementById('pokerArea');
-  reset();
+  init();
   // 为三个按钮分别绑定处理函数
   document.getElementById('reset').onclick = reset;
 
   document.getElementById('send').onclick = sendCard;
 
-  // 发牌函数
+  // 发牌函数，发几张牌
   function sendCard () {
-    // TODO:
+    // 就是在52张牌中，随机选出user.length * 5张牌
+    reset();
+    let count = users.length * 5;
+
+    let theCards = [];
+    for(let i=0; i<count; i++) {
+      let index = Math.floor(Math.random()*cards.length);
+      let theCard = cards[index];
+      theCards.push(theCard);
+      cards.splice(index,1);
+    }
+
+    console.log(theCards);
+
+    // 将图片显示
+    let imgs = document.getElementsByTagName('img');
+    for(let i=0; i<imgs.length; i++) {
+      imgs[i].setAttribute('src', 'image/'+theCards[i]+'.jpg');
+    }
+
   }
 
   // 重置函数，将所有牌清空
-  function reset () {
+  function init () {
     for (let i=0; i<users.length; i++) {
       let pokerDiv = document.createElement('div');
       pokerDiv.className = 'poker';
@@ -43,6 +62,18 @@ window.onload = () => {
         pokerDiv.appendChild(cardDiv);
       }
       pokerArea.appendChild(pokerDiv);
+    }
+  }
+
+  // reset 两个任务：清空img的src属性，将牌重新变成52张
+  function reset () {
+    // 将牌重新变成52张
+    cards = Object.keys(String(new Array(53)));
+
+    // 清除img的src属性
+    let imgs = document.getElementsByTagName('img');
+    for(let i=0; i<imgs.length; i++) {
+      imgs[i].removeAttribute('src');
     }
   }
 }
