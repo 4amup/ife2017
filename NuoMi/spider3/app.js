8d9c8b933c0e72e89f139d78adac27837a7bf8dd
wozhizuipc <490398311@qq.com>
2017-07-20 17:32:47 +0800

    update 利用query传参数。

--- NuoMi/spider3/app.js
+++ NuoMi/spider3/app.js
@@ -1,8 +1,17 @@
 const http = require('http')
+const url = require('url')
 const mongoose = require('mongoose')
 
 let app = http.createServer((req, res) => {
-  console.log('request received')
+  // 利用url解析出参数
+  if(req.url !== '/favicon.ico') {
+    let parseurl = url.parse(req.url, true);
+    console.log('request received')
+    console.log(parseurl);
+    // 然后取出query参数来
+    // todo
+    // 做一个图形化界面，输入参数后提交
+  }
   res.writeHead(200, {"Content-Type": "text/plain"})
   res.write("hello world")
   res.end()
